1f63826d72e617806efd2a0af62d3af7c08c8de1
John Ginnane <John.Ginnane1@Gmail.com>
2025-12-05 23:47:54 +0000

    okay, extracting fuel prices from page

--- fuel.js
+++ fuel.js
@@ -2,30 +2,47 @@ const http      = require("node:http");
 const https     = require("node:https");
 const axios     = require('axios');
 const cheerio   = require('cheerio');
+const regexMyLPGFuel = new RegExp("([0-9.]+)");
 
-const httpAgent = new http.Agent({ keepAlive: true });
-const httpsAgent = new https.Agent({ keepAlive: true });
-const agentSelector = function(_parsedURL) {
-    if (_parsedURL.protocol == 'http:') {
-        return httpAgent;
-    } else {
-        return httpsAgent;
-    }
-}
-
-async function getFuelPrice() {
+async function getFuelPrice(country) {
     console.log("start fuel price");
     const url = "https://www.mylpg.eu/stations/ireland/prices/";
 
     axios.get(url).then((resp) => {
         const $ = cheerio.load(resp.data);
+        const headers = [];
         const data = [];
         
-        $("span").each((k, v) => {
-            data.push($(v).text());
+        // Find headers first
+        $("tbody").find("tr").find("th").each((k, v) => {
+            var fuelType = $(v).text().trim().toLowerCase();
+
+            // Convert to our lingo
+            if (fuelType == "unleaded") {
+                fuelType = "petrol"
+            }
+
+            headers.push(fuelType);
         });
 
-        console.log(data);
+        // Then find values
+        $("tbody").find("tr").find("span").each((k, v) => {
+            var fuelPrice = $(v).text();
+            var priceMatches = fuelPrice.match(regexMyLPGFuel);
+
+            if (priceMatches.length > 0) {
+                data.push(priceMatches[0]);
+            } else {
+                data.push("0");
+            }
+        });
+
+        var i = 0;
+
+        while (i < headers.length && i < data.length) {
+            console.log(`${headers[i]}\t - ${data[i]}`);
+            i++;
+        }
     }).catch((err) => {
         console.log("Error fetch page:");
         console.error(err);
